e643120fc34aa90f0bde4d164660ae825ec26e89
Muhammad Bin Khalid <chmuhammadbinkhalid28@gmail.com>
2024-11-30 13:06:55 +0500

    Update

--- Assets/versioncheck.cs
+++ Assets/versioncheck.cs
@@ -6,32 +6,76 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using Newtonsoft.Json.Linq;
 using System.Text.RegularExpressions;
+using TMPro;
+using UnityEngine.UI;
+using System.Text.RegularExpressions;
+
 public class versioncheck : MonoBehaviour
 {
-    static string GithubReleaseVersionNumber;
     public GameObject VersionNews;
-    // Start is called before the first frame update
+
+    public TMP_InputField detailText;
+    public TMP_Text versionText;
     async void Start()
-    { 
-        CheckVersion();
+    {
+        await CheckVersion();
     }
 
-    // Update is called once per frame
-    async void CheckVersion()
+    // Convert Markdown to TextMeshPro Rich Text
+
+    string ConvertMarkdownToRichText(string markdown)
     {
-        string githubVersion = await RetrivrVersionNumberFromGithub();
+        // Remove comments between <!-- and -->
+        markdown = Regex.Replace(markdown, @"<!--.*?-->", string.Empty, RegexOptions.Singleline);
+
+        // Convert bold text (e.g., **bold** or __bold__)
+        markdown = Regex.Replace(markdown, @"(\*\*|__)(.*?)\1", "<b>$2</b>");
+
+        // Convert italic text (e.g., *italic* or _italic_)
+        markdown = Regex.Replace(markdown, @"(\*|_)(.*?)\1", "<i>$2</i>");
+
+        // Convert links (e.g., [link](url))
+        markdown = Regex.Replace(markdown, @"\[(.*?)\]\((.*?)\)", "<color=#0000EE><u>$1</u></color>");
+
+        // Convert headers (e.g., # Header -> <size=24>Header</size>)
+        markdown = Regex.Replace(markdown, @"^(#{1,6})\s*(.*?)$", match =>
+        {
+            int headerLevel = match.Groups[1].Length;
+            int fontSize = 24 - (headerLevel * 2);
+            return $"<size={fontSize}>{match.Groups[2].Value}</size>";
+        }, RegexOptions.Multiline);
+
+        // Convert newlines to line breaks
+        markdown = markdown.Replace("\n", "<br>");
+
+        return markdown;
+    }
+
+    async Task CheckVersion()
+    {
+        string githubVersion = await RetrieveVersionNumberFromGithub();
         string appVersion = Application.version;
 
-        if (githubVersion != null)
+        if (!string.IsNullOrEmpty(githubVersion))
         {
-            if (githubVersion != appVersion)
+            try
             {
-                VersionNews.SetActive(true);
-                Debug.Log($"A new version is available: {githubVersion}. Current version: {appVersion}");
+                Version appVer = new Version(appVersion);
+                Version gitVer = new Version(githubVersion);
+
+                if (gitVer > appVer)
+                {
+                    VersionNews.SetActive(true);
+                    Debug.Log($"A new version is available: {githubVersion}. Current version: {appVersion}");
+                }
+                else
+                {
+                    Debug.Log("You are using the latest version.");
+                }
             }
-            else
+            catch (Exception ex)
             {
-                Debug.Log("You are using the latest version.");
+                Debug.Log($"Error parsing version numbers: {ex.Message}");
             }
         }
         else
@@ -40,52 +84,44 @@ public class versioncheck : MonoBehaviour
         }
     }
 
-    async Task<string> RetrivrVersionNumberFromGithub()
+    async Task<string> RetrieveVersionNumberFromGithub()
     {
-        string repoOwner = "MIbnEKhalid";
-        string repoName = "UnilibUnity";
-        string url = $"https://api.github.com/repos/{repoOwner}/{repoName}/releases/latest";
+        string url = "https://api.github.com/repos/MIbnEKhalid/UnilibUnity/releases/latest";
 
         using (HttpClient client = new HttpClient())
         {
-            // Add headers
             client.DefaultRequestHeaders.Add("User-Agent", "CSharp-App");
             client.DefaultRequestHeaders.Add("Accept", "application/vnd.github.v3+json");
 
             try
             {
-                // Fetch response
                 HttpResponseMessage response = await client.GetAsync(url);
                 response.EnsureSuccessStatusCode();
 
-                // Parse JSON
                 string responseBody = await response.Content.ReadAsStringAsync();
                 JObject release = JObject.Parse(responseBody);
 
-                // Extract details
                 string releaseName = release["name"]?.ToString();
-                string tagName = release["tag_name"]?.ToString();
-                string releaseUrl = release["html_url"]?.ToString();
+                Debug.Log($"Latest Release: {releaseName}");
+                versionText.text = releaseName;
+                string releaseDate = release["published_at"]?.ToString();
+                Debug.Log(releaseDate);
 
-                //Debug.Log($"Latest Release: {releaseName}");
-                //Debug.Log($"Tag: {tagName}");
-                //Debug.Log($"URL: {releaseUrl}");
-
+                string releaseDetails = release["body"]?.ToString();
+                Debug.Log("releaseDetails: " + releaseDetails);
+                detailText.text = ConvertMarkdownToRichText(releaseDetails); ;
+;
                 string pattern = @"\[(.*?)\]";
                 Match match = Regex.Match(releaseName, pattern);
 
                 if (match.Success)
                 {
-                    string versionNumber = match.Groups[1].Value;
-                    GithubReleaseVersionNumber = versionNumber;
-                //    Debug.Log($"Extracted Version Number: {GithubReleaseVersionNumber}");
-                    return GithubReleaseVersionNumber;
+                    return match.Groups[1].Value;
                 }
                 else
                 {
                     Debug.Log("No version number found in the string.");
                 }
-
             }
             catch (HttpRequestException e)
             {
